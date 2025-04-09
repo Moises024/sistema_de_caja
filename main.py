0da527c9aece4bd6996cd33b3b7c72a7cf149c40
Moises024 <moisesrzb@gmail.com>
2025-04-09 11:52:21 -0400

    feat/actualizacion de las ventanas

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from PyQt6.QtWidgets import QApplication,QMainWindow,QSizePolicy
-from PyQt6 import uic 
+from PyQt6.uic import loadUi 
 import sys
 
 class Ventana(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi("login.ui", self)
-        self.login_label.setSizePolicy(QSizePolicy.Policy.Expanding,QSizePolicy.Policy.Expanding)
-    def msj(self,id):
-        if id ==1:
-            print("hola mundo")
-        else:
-            print(self.input_name.text())
- 
+        self.login = loadUi("./ui/login.ui", self)
+        self.almacen = loadUi("./ui/almacen.ui")
+        self.caja = loadUi("./ui/caja.ui")
+        self.eliminar = loadUi("./ui/eliminar_articulo.ui")
+        self.agregar = loadUi("./ui/agregar_articulo.ui")
+        
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     ventana = Ventana()
